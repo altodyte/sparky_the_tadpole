7ba6379d230875f2534fe177653e8948859b06ac
Siddharth Singal <ssingal05@gmail.com>
2014-12-13 00:50:50 -0500

    updated receive comms

--- PoE Tadpole/receive_comms/receive_comms.ino
+++ PoE Tadpole/receive_comms/receive_comms.ino
@@ -19,14 +19,14 @@ void receive_comms()
     buffer[i] = (char)ch;
   }
  
-  String str1 = find_next(buffer[0],4);
-  float freq = atof(find_next(buffer[5]*,4).c_str());
-  amp0 = atoi(find_next(*(buffer[10]),2).c_str());
-  amp1 = atoi(find_next(*(buffer[13]),2).c_str());
-  amp2 = atoi(find_next(*(buffer[16]),2).c_str());
-  phase1 = atof(find_next(*(buffer[19]),5).c_str());
-  phase2 = atof(find_next(*(buffer[24]),5).c_str());
-  mode = atoi(find_next(*(buffer[29]),2).c_str());
+  String str1 = find_next(buffer,4);
+  freq = atof(find_next(&(buffer[5]),4).c_str());
+  amp0 = atoi(find_next(&(buffer[10]),2).c_str());
+  amp1 = atoi(find_next(&(buffer[13]),2).c_str());
+  amp2 = atoi(find_next(&(buffer[16]),2).c_str());
+  phase1 = atof(find_next(&(buffer[19]),5).c_str());
+  phase2 = atof(find_next(&(buffer[25]),5).c_str());
+  mode = atoi(find_next(&(buffer[31]),2).c_str());
   
   if (strncmp(buffer, "STOP", 4)){
     state = STOP;}

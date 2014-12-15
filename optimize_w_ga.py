07c3e046f5a35514b02aa0176777695bb73c3bd7
Michael Bocamazo <michael.bocamazo@students.olin.edu>
2014-12-14 19:14:06 -0500

    optimization written and tested

--- optimize_w_ga.py
+++ optimize_w_ga.py
@@ -10,12 +10,17 @@ import csv
 import os
 import numpy as np
 
-"""float freq = 2;
+"""
+some initial params (tho look in 'learndata.csv' for 4 initials)
+float freq = 2;
 float amp0 = 60;
 float amp1 = 80;
 float amp2 = 80;
 float phase1 = 1.57;
-float phase2 = 1.57; //phases should be relative"""
+float phase2 = 1.57; //phases should be relative
+selection: 0-.25 is .6, .25-.5 is .3, .5 - 1 is .1
+mutation: f: 2, amp: 20, ph: pi/4
+"""
 
 def write_back(parameter_list, speed, heading_change, euclid_dist, time_diff):
     """appends new run to data file, parameter_list should be a list of length 6, others should be floats"""
@@ -31,8 +36,9 @@ def get_new_params():
     prev_data = np.genfromtxt('learndata.csv', delimiter=',',dtype=float)
     #dtype = [('freq',float),('amp0',float),('amp1',float),('amp2',float),('ph1',float),('ph2',float),('speed',float),('heading_change',float),('euclid_dist',float),('time_diff',float)]
     #b = np.array(prev_data, dtype = dtype)
+    #instead of this working nicely with fields, just have speed as the first col
     sorted_array= np.sort(prev_data, axis = 0)
-    num_runs = np.shape(sorted_array)[0]
+    num_runs = np.shape(sorted_array)[0]    
     ind_base = weighted_draw(num_runs)
     ind_second = weighted_draw(num_runs)
     params_base = sorted_array[ind_base, 4:10]
@@ -50,7 +56,7 @@ def mutate_params(gene, num_runs):
         gene[0] = range_coerce(gene[0],0.2,6)
     for i in range(1,4):
         if random.random() < mut_prob:
-            gene[i] += 2*(random.random()-0.5)*mut_mag*20
+            gene[i] += 2*(random.random()-0.5)*mut_mag*40
             gene[i] = range_coerce(gene[i],20,90)
     for i in range(4,6):
         if random.random() < mut_prob:
@@ -58,7 +64,7 @@ def mutate_params(gene, num_runs):
             gene[i] = range_coerce(gene[i],-math.pi, math.pi)
     return gene
 
-def range_coerce(x, upper, lower):
+def range_coerce(x, lower, upper):
     if x> upper:
         return upper
     elif x < lower:
@@ -69,7 +75,7 @@ def anneal_sched_mut_mag(generation_num):
     """calculates appropriate ratio of magnitude of mutation given number of runs"""
     mut_mag = None    
     if 0 <= generation_num < 40:
-        mut_mag = 1-generation_num/40.0
+        mut_mag = (1-float(generation_num)/40.0)
     if 40<= generation_num < 50:
         mut_mag = .2
     if 50<= generation_num:
@@ -98,36 +104,11 @@ def recombine_params(params_base, params_second):
 def weighted_draw(num_runs):
     """returns index of previous run to use, based on weighting of different sections"""
     quartile = math.ceil(float(num_runs)/4.0)
-    half = math.ceil(float(num_runs)/2.0)
+    half = math.ceil(float(num_runs)/2.0)    
     k = random.random()
-    if k<.6:
-        return random.randint(0,quartile)
-    if k<.9:
-        return random.randint(quartile,half)
-    return random.randint(half,num_runs-1)
+    if k<.6:        
+        return random.randint(num_runs-1-quartile,num_runs-1) #would like to reverse sort, but not built in
+    if k<.9:        
+        return random.randint(half,num_runs-1-quartile)
+    return random.randint(0,half-1)
 
-    """
-    dtype = [('name', 'S10'), ('height', float), ('age', int)]
-    values = [('Arthur', 1.8, 41), ('Lancelot', 1.9, 38), ('Galahad', 1.7, 38)]
-    a = np.array(values, dtype=dtype)       # create a structured array
-    np.sort(a, order='height')                        
-    #array([('Galahad', 1.7, 38), ('Arthur', 1.8, 41), ('Lancelot', 1.8999999999999999, 38)], dtype=[('name', '|S10'), ('height', '<f8'), ('age', '<i4')])    
-
-    recom prob 
-    mut prob
-    mut mag calc 
-    mut mag list 
-    generations? - just length of list
-    ...histogram of past 
-    and other measures of central tendency of past runs, statistical measures, etc
-    annealing Schedule
-    mutation probs over all, tuple of mutation mags over all parameters, should be different mags for different params
-    selection: 0-.25 is .6, .25-.5 is .3, .5 - 1 is .1
-    #    self.AI_list.sort(key=lambda x: x.tournament_score, reverse=True)
-
-    sort
-    draw 2
-    recombine func 
-    mutate func 
-    return
-    """
